b73fa12a58e815635a1f6251d727eebe2407595a
Rahul <kumar.rahul@iitgn.ac.in>
2022-11-05 02:19:51 +0530

    Hide Blog and Education pages

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing", {
+    strings: ["", "Summer Camp", "Sanjeevani Medical Camp", "Cloth Distribution","Fruits Distribution", "Sweet Distribution"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+    })
